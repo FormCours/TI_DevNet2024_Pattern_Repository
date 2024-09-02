87c440a1cce6dc634b02989e0c464c64d4b9d226
Aurelien St <aurelien.strimelle@bstorm.be>
2024-09-02 09:42:39 +0200

    Ajoutez des fichiers projet.

---
+++ Demo_Pattern_Repository.ScriptSQL/Tables/Animal.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Animal]
+(
+	[Id] INT IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Domesticated] BIT NOT NULL,
+	[LifeExpectancy] INT NULL,
+	[FamiliaId] INT NOT NULL,
+
+	CONSTRAINT [PK_Animal] PRIMARY KEY([Id]),
+	CONSTRAINT [UK_Animal] UNIQUE([Name], [Domesticated]),
+	CONSTRAINT [FK_Animal__Familia] FOREIGN KEY([FamiliaId]) REFERENCES [Familia]([Id])
+)
---
+++ Demo_Pattern_Repository.ScriptSQL/Tables/Familia.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Familia]
+(
+	[Id] INT IDENTITY,
+	[Name] NVARCHAR(50) NOT NULL,
+	[Description] NVARCHAR(4000),
+
+	CONSTRAINT [PK_Familia] PRIMARY KEY ([Id]),
+	CONSTRAINT [UK_Familia] UNIQUE ([Name])
+)
